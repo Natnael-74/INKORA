bf51a8ac13f5863a434a09132de326cbee310b63
Natnael Endale <n87294578@gmail.com>
2026-04-02 20:00:42 +0300

    add the google provider

---
+++ next-blog-starter/src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  //secret: process.env.NEXTAUTH_SECRET,
+};
+
+//export default NextAuth(authOptions);
