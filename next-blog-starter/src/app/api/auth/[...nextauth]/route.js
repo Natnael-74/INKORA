1edefd3135c620d8bcfd3050ace5a49ccb693b17
Natnael Endale <n87294578@gmail.com>
2026-04-02 20:01:02 +0300

    add the github provider

--- next-blog-starter/src/app/api/auth/[...nextauth]/route.js
+++ next-blog-starter/src/app/api/auth/[...nextauth]/route.js
@@ -8,6 +8,10 @@ export const authOptions = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
   ],
   //secret: process.env.NEXTAUTH_SECRET,
 };
