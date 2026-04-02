b6d2e6d57c4bfbec40041cb53199b4779a40d2c7
Natnael Endale <n87294578@gmail.com>
2026-04-02 20:01:27 +0300

    add the env variables

--- next-blog-starter/src/app/api/auth/[...nextauth]/route.js
+++ next-blog-starter/src/app/api/auth/[...nextauth]/route.js
@@ -13,7 +13,7 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  //secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 //export default NextAuth(authOptions);
