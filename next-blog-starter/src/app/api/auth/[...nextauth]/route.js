5331866b2deb94b0bf855c15b344be106991d46d
Natnael Endale <n87294578@gmail.com>
2026-04-03 21:47:25 +0300

    arrange the config

--- next-blog-starter/src/app/api/auth/[...nextauth]/route.js
+++ next-blog-starter/src/app/api/auth/[...nextauth]/route.js
@@ -1,19 +1,4 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from "next-auth/providers/github";
-
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import { authOptions } from "@/utils/auth";
 
 export default NextAuth(authOptions);
