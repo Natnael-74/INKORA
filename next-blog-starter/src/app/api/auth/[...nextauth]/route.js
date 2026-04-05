574415e697e3110851a2df85e62d1e21efdb8daf
Natnael Endale <n87294578@gmail.com>
2026-04-05 12:20:44 +0300

    correct the auth

--- next-blog-starter/src/app/api/auth/[...nextauth]/route.js
+++ next-blog-starter/src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,3 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/utils/auth";
+import { handlers } from "@/utils/auth";
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+export const { GET, POST } = handlers;
